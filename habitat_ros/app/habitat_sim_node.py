47b4f49fb1ada323d3df8debaafb2a32104340b2
Giuliano Albanese <albanese.giuliano97@gmail.com>
2022-04-26 17:48:21 +0200

    Move ros stuff out of async simulator

--- habitat_ros/app/habitat_sim_node.py
+++ habitat_ros/app/habitat_sim_node.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 
+from threading import Thread
 
+import cv2
+import numpy as np
 import rospy
 import tf
-from geometry_msgs.msg import Twist, Pose
+import tf2_ros
+from cv_bridge import CvBridge
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import (
+    Twist,
+    Point,
+    Pose,
+    PoseStamped,
+    Quaternion,
+    Transform,
+    TransformStamped,
+)
+from sensor_msgs.msg import Image, CameraInfo
+from std_msgs.msg import Header
 from trajectory_msgs.msg import MultiDOFJointTrajectory, MultiDOFJointTrajectoryPoint
 
 from habitat_ros.async_simulator import AsyncSimulator
@@ -12,11 +28,43 @@ from habitat_ros.pid_position_controller import (
     PIDPositionControllerParameters,
 )
 from habitat_ros.utils.conversions import (
+    numpy_to_vector3,
     vector3_to_numpy,
+    vec_habitat_to_ros,
     vec_ros_to_habitat,
     quaternion_to_numpy,
     quat_ros_to_habitat,
 )
+
+
+def make_depth_camera_info_msg(header, height, width):
+    r"""
+    Create camera info message for depth camera.
+    :param header: header to create the message
+    :param height: height of depth image
+    :param width: width of depth image
+    :returns: camera info message of type CameraInfo.
+    """
+    # code modifed upon work by Bruce Cui
+    camera_info_msg = CameraInfo()
+    camera_info_msg.header = header
+    fx, fy = width / 2, height / 2
+    cx, cy = width / 2, height / 2
+
+    camera_info_msg.width = width
+    camera_info_msg.height = height
+    camera_info_msg.distortion_model = "plumb_bob"
+    camera_info_msg.K = np.float32([fx, 0, cx, 0, fy, cy, 0, 0, 1])
+    camera_info_msg.D = np.float32([0, 0, 0, 0, 0])
+    camera_info_msg.P = [fx, 0, cx, 0, 0, fy, cy, 0, 0, 0, 1, 0]
+    return camera_info_msg
+
+
+def convert_instance_to_semantic_segmentation(segmentation, instance_id_to_class_id):
+    ids, inv = np.unique(segmentation, return_inverse=True)
+    return np.array(
+        [instance_id_to_class_id[x] if x in instance_id_to_class_id else 0 for x in ids]
+    )[inv].reshape(segmentation.shape)
 
 
 class HabitatSimNode:
@@ -48,12 +96,46 @@ class HabitatSimNode:
             use_embodied_agent=self.use_embodied_agent,
         )
 
-        # Instantiate and configure position controller to track pose
-        # and trajectory commands
+        # TODO: make these configurable or read from rosparam server
+        self.global_frame_name = "world"
+        self.agent_frame_name = "base_link"
+        self.sensor_frame_name = "depth_cam"
+
+        self.cv_bridge = CvBridge()
+
+        # Instantiate and configure position controller to track pose commands
         # TODO: read pid controller params from ros
         self.position_controller = PIDPositionController(
             PIDPositionControllerParameters()
         )
+
+        # Configure publishers to sensor topics
+        self.rgb_pub = rospy.Publisher("~rgb", Image, queue_size=100)
+        self.depth_pub = rospy.Publisher("~depth", Image, queue_size=100)
+        self.instance_pub = rospy.Publisher("~instance", Image, queue_size=100)
+        self.semantic_pub = rospy.Publisher("~semantic", Image, queue_size=100)
+        # fmt: off
+        self.rgb_3rd_person_pub = rospy.Publisher("~rgb_3rd_person", Image, queue_size=100)
+        # fmt: on
+        self.pose_pub = rospy.Publisher("~pose", PoseStamped, queue_size=100)
+        self.odom_pub = rospy.Publisher("~odom", Odometry, queue_size=100)
+        self.tf_broadcaster = tf.TransformBroadcaster()
+
+        # Configure static transform broadcaster for static transforms
+        self.static_tf_broadcaster = tf2_ros.StaticTransformBroadcaster()
+
+        self.sensor_to_agent_transform = TransformStamped(
+            header=Header(stamp=rospy.Time.now(), frame_id=self.agent_frame_name),
+            child_frame_id=self.sensor_frame_name,
+            transform=Transform(
+                translation=numpy_to_vector3(
+                    vec_habitat_to_ros(np.array([0, self.sensor_height, 0]))
+                ),
+                # The ros convention for camera frame orientation is z forward, y down, x right
+                rotation=Quaternion(0.5, -0.5, 0.5, -0.5),
+            ),
+        )
+        self.static_tf_broadcaster.sendTransform([self.sensor_to_agent_transform])
 
         # Configure subscriber for command topics
         self.cmd_vel_sub = rospy.Subscriber(
@@ -69,6 +151,20 @@ class HabitatSimNode:
             self.cmd_pose_callback,
             queue_size=10,
         )
+
+    def cv2_to_depthmsg(self, depth_img: np.ndarray) -> Image:
+        r"""
+        Converts a Habitat depth image to a ROS Image message.
+        """
+        if len(depth_img.shape) > 2:
+            depth_img_in_m = np.squeeze(depth_img, axis=2)
+        else:
+            depth_img_in_m = depth_img
+        depth_msg = self.cv_bridge.cv2_to_imgmsg(
+            depth_img_in_m.astype(np.float32),
+            encoding="passthrough",
+        )
+        return depth_msg
 
     def cmd_vel_callback(self, cmd_vel_msg: Twist):
         # Convert from ros to habitat coordinate convention
@@ -90,15 +186,97 @@ class HabitatSimNode:
         # FIXME: not thread safe?
         self.position_controller.set_target(target_position, target_yaw)
 
-    def simulate(self):
+    def _publish_odometry_and_sensor_observations(self):
+        (
+            position,
+            orientation,
+            observations,
+        ) = self.async_sim.get_agent_pose_and_sensor_observations()
+
+        # Wrap observations into messages
+
+        # Use timer timestamp
+        timestamp = rospy.Time.now()
+
+        # Common header - so that all the observations have the same timestamp
+        sensor_msgs_header = Header(
+            stamp=timestamp,
+            frame_id=self.sensor_frame_name,
+        )
+
+        rgb = cv2.cvtColor(observations["color_sensor"], cv2.COLOR_RGBA2BGR)
+        rgb_msg = self.cv_bridge.cv2_to_imgmsg(rgb.astype(np.uint8), encoding="bgr8")
+        rgb_msg.header = sensor_msgs_header
+
+        rgb_3rd_person = cv2.cvtColor(
+            observations["color_sensor_3rd_person"], cv2.COLOR_RGBA2BGR
+        )
+        rgb_3rd_person_msg = self.cv_bridge.cv2_to_imgmsg(
+            rgb_3rd_person.astype(np.uint8), encoding="bgr8"
+        )
+        rgb_3rd_person_msg.header = sensor_msgs_header
+
+        depth = observations["depth_sensor"]
+        depth_msg = self.cv2_to_depthmsg(depth)
+        depth_msg.header = sensor_msgs_header
+
+        instance = observations["semantic_sensor"]
+        instance_msg = self.cv_bridge.cv2_to_imgmsg(instance.astype(np.uint16))
+        instance_msg.header = sensor_msgs_header
+
+        semantic = convert_instance_to_semantic_segmentation(
+            instance,
+            self.async_sim.instance_id_to_class_id,  # Hack - TODO: implement getter
+        )
+        semantic_msg = self.cv_bridge.cv2_to_imgmsg(semantic.astype(np.uint16))
+        semantic_msg.header = sensor_msgs_header
+
+        pose_msg = PoseStamped()
+        pose_msg.header = Header(stamp=timestamp, frame_id=self.agent_frame_name)
+        pose_msg.pose.position = Point(*position)
+        pose_msg.pose.orientation = Quaternion(*orientation)
+
+        odom_msg = Odometry()
+        odom_msg.header = Header(stamp=timestamp, frame_id=self.global_frame_name)
+        odom_msg.child_frame_id = self.agent_frame_name
+        odom_msg.pose.pose.position = Point(*position)
+        odom_msg.pose.pose.orientation = Quaternion(*orientation)
+
+        # Publish observations
+        self.rgb_pub.publish(rgb_msg)
+        self.depth_pub.publish(depth_msg)
+        self.rgb_3rd_person_pub.publish(rgb_3rd_person_msg)
+        self.instance_pub.publish(instance_msg)
+        self.semantic_pub.publish(semantic_msg)
+        self.pose_pub.publish(pose_msg)
+        self.odom_pub.publish(odom_msg)
+        self.tf_broadcaster.sendTransform(
+            translation=position,
+            rotation=orientation,
+            time=timestamp,
+            child=self.agent_frame_name,
+            parent=self.global_frame_name,
+        )
+
+    def _start_simulator_and_controller_threads(self):
+        # Start simulator on a separate thread
+        self.async_sim.start()
+
+        # TODO: start controller thread
+
+    def run(self):
+        r"""
+        Start loop in which at every iteration sensor observations
+        are published. Blocking.
+        """
         try:
+            self._start_simulator_and_controller_threads()
             if self.wait:
                 rospy.sleep(10.0)
             # Start the simulator in a separate thread
-            self.async_sim.start()
             _r = rospy.Rate(self.sensor_rate)
             while not rospy.is_shutdown():
-                self.async_sim.publish_sensor_observations_and_odometry()
+                self._publish_odometry_and_sensor_observations()
                 _r.sleep()
         except Exception as e:
             print(str(e))
@@ -106,4 +284,4 @@ class HabitatSimNode:
 
 if __name__ == "__main__":
     habitat_sim_node = HabitatSimNode("habitat_sim_node")
-    habitat_sim_node.simulate()
+    habitat_sim_node.run()
--- habitat_ros/src/habitat_ros/async_simulator.py
+++ habitat_ros/src/habitat_ros/async_simulator.py
@@ -1,53 +1,13 @@
 from threading import Thread, Lock
-from typing import Optional
+from typing import Optional, Tuple
 
-from cv_bridge import CvBridge
 import habitat_sim
 import magnum
 import numpy as np
+import quaternion
 import rospy
-import tf
-import tf2_ros
-from geometry_msgs.msg import (
-    Point,
-    Quaternion,
-    PoseStamped,
-    TransformStamped,
-    Transform,
-)
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Image, CameraInfo
-from std_msgs.msg import Header
 
-from .utils.conversions import (
-    numpy_to_vector3,
-    vec_habitat_to_ros,
-    quat_habitat_to_ros,
-    REPLICA_CLASS_ID_TO_NYU40,
-)
-
-
-def make_depth_camera_info_msg(header, height, width):
-    r"""
-    Create camera info message for depth camera.
-    :param header: header to create the message
-    :param height: height of depth image
-    :param width: width of depth image
-    :returns: camera info message of type CameraInfo.
-    """
-    # code modifed upon work by Bruce Cui
-    camera_info_msg = CameraInfo()
-    camera_info_msg.header = header
-    fx, fy = width / 2, height / 2
-    cx, cy = width / 2, height / 2
-
-    camera_info_msg.width = width
-    camera_info_msg.height = height
-    camera_info_msg.distortion_model = "plumb_bob"
-    camera_info_msg.K = np.float32([fx, 0, cx, 0, fy, cy, 0, 0, 1])
-    camera_info_msg.D = np.float32([0, 0, 0, 0, 0])
-    camera_info_msg.P = [fx, 0, cx, 0, 0, fy, cy, 0, 0, 0, 1, 0]
-    return camera_info_msg
+from .utils.conversions import REPLICA_CLASS_ID_TO_NYU40
 
 
 def make_agent_cfg(image_width: int, image_height: int, sensor_height: float):
@@ -152,42 +112,6 @@ class AsyncSimulator(Thread):
         self.enable_physics = enable_physics
         self.is_replica = is_replica
 
-        # TODO: make these configurable or read from rosparam server
-        self.global_frame_name = "world"
-        self.agent_frame_name = "base_link"
-        self.sensor_frame_name = "depth_cam"
-
-        self.cv_bridge = CvBridge()
-
-        # Configure publishers to sensor topics
-        self.rgb_pub = rospy.Publisher("~rgb", Image, queue_size=100)
-        self.depth_pub = rospy.Publisher("~depth", Image, queue_size=100)
-        self.instance_pub = rospy.Publisher("~instance", Image, queue_size=100)
-        self.semantic_pub = rospy.Publisher("~semantic", Image, queue_size=100)
-        # fmt: off
-        self.rgb_3rd_person_pub = rospy.Publisher("~rgb_3rd_person", Image, queue_size=100)
-        self.camera_info_pub = rospy.Publisher("~camera_info", CameraInfo, queue_size=100)
-        # fmt: on
-        self.pose_pub = rospy.Publisher("~pose", PoseStamped, queue_size=100)
-        self.odom_pub = rospy.Publisher("~odom", Odometry, queue_size=100)
-        self.tf_broadcaster = tf.TransformBroadcaster()
-
-        # Configure static transform broadcaster for static transforms
-        self.static_tf_broadcaster = tf2_ros.StaticTransformBroadcaster()
-
-        self.sensor_to_agent_transform = TransformStamped(
-            header=Header(stamp=rospy.Time.now(), frame_id=self.agent_frame_name),
-            child_frame_id=self.sensor_frame_name,
-            transform=Transform(
-                translation=numpy_to_vector3(
-                    vec_habitat_to_ros(np.array([0, self.sensor_height, 0]))
-                ),
-                # The ros convention for camera frame orientation is z forward, y down, x right
-                rotation=Quaternion(0.5, -0.5, 0.5, -0.5),
-            ),
-        )
-        self.static_tf_broadcaster.sendTransform([self.sensor_to_agent_transform])
-
         # Generate sim config
         self.cfg = habitat_sim.Configuration(
             make_sim_cfg(
@@ -263,111 +187,30 @@ class AsyncSimulator(Thread):
         self.vel_control_requested_duration = None
         self.vel_control_duration = 0.0
 
-    def cv2_to_depthmsg(self, depth_img: np.ndarray) -> Image:
+    def _get_agent_pose(self) -> Tuple[np.ndarray, np.ndarray]:
         r"""
-        Converts a Habitat depth image to a ROS Image message.
+        Get current agent position and orientation as numpy arrays.
+        Not thread safe.
         """
-        if len(depth_img.shape) > 2:
-            depth_img_in_m = np.squeeze(depth_img, axis=2)
-        else:
-            depth_img_in_m = depth_img
-        depth_msg = self.cv_bridge.cv2_to_imgmsg(
-            depth_img_in_m.astype(np.float32),
-            encoding="passthrough",
+        agent_position = self.agent.state.position
+        agent_orientation = np.roll(
+            quaternion.as_float_array(self.agent.state.rotation), -1
         )
-        return depth_msg
+        return agent_position, agent_orientation
 
-    def _convert_instance_to_semantic_segmentation(self, segmentation):
-        ids, inv = np.unique(segmentation, return_inverse=True)
-        return np.array(
-            [
-                self.instance_id_to_class_id[x]
-                if x in self.instance_id_to_class_id
-                else 0
-                for x in ids
-            ]
-        )[inv].reshape(segmentation.shape)
-
-    def publish_sensor_observations_and_odometry(self):
-        # Acquire the lock to get the sensor info then release it
-        # so the simulation thread is not waiting for too long
+    def get_agent_pose(self) -> Tuple[np.ndarray, np.ndarray]:
+        r"""
+        Thread-safe public interface to get the current agent pose.
+        """
         with self.sim_lock:
-            timestamp = rospy.Time.now()
+            return self._get_agent_pose()
 
-            # Collect sensor observations
+    def get_agent_pose_and_sensor_observations(self):
+        with self.sim_lock:
             observations = self.sim.get_sensor_observations()
+            agent_position, agent_orientation = self._get_agent_pose()
 
-            # Get agent pose info
-            agent_orientation = quat_habitat_to_ros(
-                np.roll(np.array(self.agent.state.rotation.components), -1),
-            )
-            agent_translation = vec_habitat_to_ros(self.agent.state.position)
-
-        # Wrap observations into messages
-
-        # Common header - so that all the observations have the same timestamp
-        sensor_msgs_header = Header(
-            stamp=timestamp,
-            frame_id=self.sensor_frame_name,
-        )
-
-        rgb = observations["color_sensor"]
-        if rgb.shape[2] > 3:
-            rgb = rgb[:, :, :3]
-        rgb_msg = self.cv_bridge.cv2_to_imgmsg(rgb.astype(np.uint8), encoding="bgr8")
-        rgb_msg.header = sensor_msgs_header
-
-        rgb_3rd_person = observations["color_sensor_3rd_person"]
-        if rgb_3rd_person.shape[2] > 3:
-            rgb_3rd_person = rgb_3rd_person[:, :, :3]
-        rgb_3rd_person_msg = self.cv_bridge.cv2_to_imgmsg(
-            rgb_3rd_person.astype(np.uint8), encoding="bgr8"
-        )
-        rgb_3rd_person_msg.header = sensor_msgs_header
-
-        depth = observations["depth_sensor"]
-        depth_msg = self.cv2_to_depthmsg(depth)
-        depth_msg.header = sensor_msgs_header
-
-        instance = observations["semantic_sensor"]
-        instance_msg = self.cv_bridge.cv2_to_imgmsg(instance.astype(np.uint16))
-        instance_msg.header = sensor_msgs_header
-
-        semantic = self._convert_instance_to_semantic_segmentation(instance)
-        semantic_msg = self.cv_bridge.cv2_to_imgmsg(semantic.astype(np.uint16))
-        semantic_msg.header = sensor_msgs_header
-
-        camera_info_msg = make_depth_camera_info_msg(
-            sensor_msgs_header, self.image_height, self.image_width
-        )
-
-        pose_msg = PoseStamped()
-        pose_msg.header = Header(stamp=timestamp, frame_id=self.agent_frame_name)
-        pose_msg.pose.position = Point(*agent_translation)
-        pose_msg.pose.orientation = Quaternion(*agent_orientation)
-
-        odom_msg = Odometry()
-        odom_msg.header = Header(stamp=timestamp, frame_id=self.global_frame_name)
-        odom_msg.child_frame_id = self.agent_frame_name
-        odom_msg.pose.pose.position = Point(*agent_translation)
-        odom_msg.pose.pose.orientation = Quaternion(*agent_orientation)
-
-        # Publish observations
-        self.rgb_pub.publish(rgb_msg)
-        self.depth_pub.publish(depth_msg)
-        self.rgb_3rd_person_pub.publish(rgb_3rd_person_msg)
-        self.camera_info_pub.publish(camera_info_msg)
-        self.instance_pub.publish(instance_msg)
-        self.semantic_pub.publish(semantic_msg)
-        self.pose_pub.publish(pose_msg)
-        self.odom_pub.publish(odom_msg)
-        self.tf_broadcaster.sendTransform(
-            translation=agent_translation,
-            rotation=agent_orientation,
-            time=timestamp,
-            child=self.agent_frame_name,
-            parent=self.global_frame_name,
-        )
+        return agent_position, agent_orientation, observations
 
     def set_vel_control(
         self,
@@ -396,7 +239,7 @@ class AsyncSimulator(Thread):
                 self.time_step,
                 cur_agent_pose,
             )
-            
+
             if self.vel_control_requested_duration is not None:
                 self.vel_control_duration += self.time_step
                 if self.vel_control_duration >= self.vel_control_requested_duration:
