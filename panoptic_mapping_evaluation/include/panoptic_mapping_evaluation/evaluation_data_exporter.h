7c17e2c2ef0ac2fb1e7f0eea2bf33c6366c1a59a
Giuliano Albanese <albanese.giuliano97@gmail.com>
2022-05-10 22:54:47 +0200

    Implement alignment to gt mesh

--- panoptic_mapping_evaluation/include/panoptic_mapping_evaluation/evaluation_data_exporter.h
+++ panoptic_mapping_evaluation/include/panoptic_mapping_evaluation/evaluation_data_exporter.h
@@ -1,10 +1,14 @@
 #ifndef PANOPTIC_MAPPING_EVALUATION_EVALUATION_DATA_EXPORTER_H_
 #define PANOPTIC_MAPPING_EVALUATION_EVALUATION_DATA_EXPORTER_H_
 
+#include <filesystem>
+#include <optional>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
-#include <panoptic_mapping/common/common.h>
-#include <panoptic_mapping/tools/planning_interface.h>
+#include <Eigen/Core>
+#include <Eigen/Dense>
 #include <panoptic_mapping_msgs/SaveLoadMap.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -14,6 +18,10 @@
 #include <panoptic_mapping/3rd_party/config_utilities.hpp>
 // clang-format on
 
+namespace panoptic_mapping {
+class SubmapCollection;
+}
+
 class EvaluationDataExporter {
  public:
   struct Config : public config_utilities::Config<Config> {
@@ -22,7 +30,16 @@ class EvaluationDataExporter {
     std::string ground_truth_pointcloud_file = "";
     std::string output_suffix = "_eval_data.txt";
     bool is_single_tsdf = true;
-
+    bool refine_alignment = false;
+    bool export_mesh = false;
+    // clang-format off
+    std::vector<double> alignment_transformation = {
+      1.0, 0.0, 0.0, 0.0,
+      0.0, 1.0, 0.0, 0.0,
+      0.0, 0.0, 1.0, 0.0,
+      0.0, 0.0, 0.0, 1.0
+    };
+    // clang-format on
     Config() { setConfigName("EvaluationDataExporter::Config"); }
 
    protected:
@@ -43,7 +60,18 @@ class EvaluationDataExporter {
   void setupMembers();
   void setupRos();
 
-  void exportEvaluationData(
+  bool exportVertexLabels(
+      const std::shared_ptr<panoptic_mapping::SubmapCollection>& submaps,
+      const Eigen::Matrix4d& alignment_transformation,
+      const std::filesystem::path& vertex_labels_file_path,
+      const std::optional<std::unordered_map<int, int>>& id_remapping =
+          std::nullopt);
+
+  bool exportMesh(
+      const std::shared_ptr<panoptic_mapping::SubmapCollection>& submaps,
+      const std::filesystem::path& mesh_file_path);
+
+  Eigen::Matrix4d computeAlignmentTransformation(
       const std::shared_ptr<panoptic_mapping::SubmapCollection>& submaps);
 
   const Config config_;
@@ -53,6 +81,8 @@ class EvaluationDataExporter {
   ros::ServiceServer export_evaluation_data_server_;
 
   pcl::PointCloud<pcl::PointXYZ>::Ptr gt_cloud_ptr_;
+  Eigen::Matrix4d default_alignment_transformation_;
+  
 };
 
 #endif  // PANOPTIC_MAPPING_EVALUATIONVALUATION_DATA_EXPORTER_H_
