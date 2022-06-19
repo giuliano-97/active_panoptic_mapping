7aee0051ec34fb00aee0900a078f65dcb7da5381
Giuliano Albanese <albanesg@student.ethz.ch>
2022-06-19 10:07:48 +0000

    Make sure non eval classes are ignored in evaluation

--- panoptic_mapping_evaluation/app/evaluation_manager_node.py
+++ panoptic_mapping_evaluation/app/evaluation_manager_node.py
@@ -4,11 +4,25 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import rospy
+from tqdm import tqdm
 
 from panoptic_mapping_msgs.srv import SaveLoadMap
 
-from panoptic_mapping_evaluation.constants import NYU40_IGNORE_LABEL
+from panoptic_mapping_evaluation.constants import (
+    NYU40_IGNORE_LABEL,
+    SCANNET_NYU40_EVALUATION_CLASSES,
+    PANOPTIC_LABEL_DIVISOR,
+)
 from panoptic_mapping_evaluation.metrics import panoptic_quality, mean_iou
+
+
+def mask_ignored_categories(vertex_panoptic_labels: np.ndarray):
+    res = np.copy(vertex_panoptic_labels)
+    category_ids = vertex_panoptic_labels // PANOPTIC_LABEL_DIVISOR
+    for c in np.unique(category_ids):
+        if c not in SCANNET_NYU40_EVALUATION_CLASSES:
+            res[category_ids == c] = NYU40_IGNORE_LABEL
+    return res
 
 
 class EvaluationManager:
@@ -39,6 +53,7 @@ class EvaluationManager:
         self.gt_vertex_labels = np.loadtxt(self.gt_vertex_labels_file_path).astype(
             np.int64
         )
+        self.gt_vertex_labels = mask_ignored_categories(self.gt_vertex_labels)
 
         rospy.wait_for_service(self.export_eval_data_srv_name)
         self.export_eval_data_srv_proxy = rospy.ServiceProxy(
@@ -51,17 +66,19 @@ class EvaluationManager:
 
         metrics_list = []
 
-        for map_file_path in map_files:
-            rospy.loginfo(f"Exporting evaluation data for {map_file_path.name}")
-            success = self.export_eval_data_srv_proxy(str(map_file_path.absolute()))
-            if not success:
-                rospy.logerror(f"Skipped.")
-
+        for map_file_path in tqdm(map_files):
             pred_vertex_labels_file_path = map_file_path.with_suffix(".txt")
+
             if not pred_vertex_labels_file_path.is_file():
-                rospy.logwarn(
-                    f"{str(pred_vertex_labels_file_path)} not found. Skipped."
-                )
+                rospy.loginfo(f"Exporting evaluation data for {map_file_path.name}")
+                try:
+                    _ = self.export_eval_data_srv_proxy(str(map_file_path.absolute()))
+                except rospy.ServiceException as se:
+                    rospy.logerr(f"Skipped.")
+                    continue
+
+            if not pred_vertex_labels_file_path.is_file():
+                rospy.logerr(f"{str(pred_vertex_labels_file_path)} not found. Skipped.")
                 continue
 
             # Load predicted vertex panoptic labels
@@ -100,17 +117,29 @@ class EvaluationManager:
 
             metrics_dict.update(
                 panoptic_quality(
-                    gt_labels=covered_gt_vertex_labels,
+                    gt_labels=covered_gt_vertex_labels
+                    if self.experiment_type == "mapping"
+                    else self.gt_vertex_labels,
                     pred_labels=rectified_pred_vertex_labels,
                 )
             )
 
             metrics_dict.update(
                 mean_iou(
-                    gt_labels=covered_gt_vertex_labels,
+                    gt_labels=covered_gt_vertex_labels
+                    if self.experiment_type == "mapping"
+                    else self.gt_vertex_labels,
                     pred_labels=rectified_pred_vertex_labels,
                 )
             )
+
+            if self.experiment_type == "planning":
+                metrics_dict.update(
+                    {
+                        "Coverage": np.count_nonzero(covered_vertices_mask)
+                        / covered_vertices_mask.size
+                    }
+                )
 
             metrics_list.append(metrics_dict)
 
