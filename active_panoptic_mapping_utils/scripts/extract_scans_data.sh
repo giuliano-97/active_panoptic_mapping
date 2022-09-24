e968b058a59d7a5db5f2edab8932fd93fe89ff5a
Giuliano Albanese <albanesg@student.ethz.ch>
2022-09-24 17:07:25 +0000

    fix extract scans data utils script

--- active_panoptic_mapping_utils/scripts/extract_scans_data.sh
+++ active_panoptic_mapping_utils/scripts/extract_scans_data.sh
@@ -10,15 +10,17 @@ fi
 
 SCANS_DIR_PATH=$1
 
-for SENS_FILE in $(find ${SCAN_DIR_PATH} -type f -name *.sens)
+for SENS_FILE in $(find ${SCANS_DIR_PATH} -type f -name "*.sens")
 do  
     SCAN_DIR="$(dirname "$SENS_FILE")"
-    python3 extract_scans_data \
+    SCAN_NAME=$(basename "$SCAN_DIR")
+    echo "Extracting scan $SCAN_NAME"
+    python3 extract_scan_data.py \
         --filename $SENS_FILE \
         --output_path $SCAN_DIR \
         --export_color \
         --export_depth \
         --export_pose \
         --export_intrinsic \
-        --image-size 640 480
+        --image_size 640 480
 done
