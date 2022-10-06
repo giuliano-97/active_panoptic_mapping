3ace2cb4720d4193f36f27f3272146e12e47117c
Giuliano Albanese <albanesg@student.ethz.ch>
2022-10-06 17:30:30 +0200

    refactor evaluation data exporter to receive gt file path

--- active_panoptic_mapping_utils/include/active_panoptic_mapping_utils/evaluation_data_exporter.h
+++ active_panoptic_mapping_utils/include/active_panoptic_mapping_utils/evaluation_data_exporter.h
@@ -9,7 +9,7 @@
 
 #include <Eigen/Core>
 #include <Eigen/Dense>
-#include <panoptic_mapping_msgs/SaveLoadMap.h>
+#include <active_panoptic_mapping_utils/ExportEvaluationData.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <ros/ros.h>
@@ -27,7 +27,6 @@ class EvaluationDataExporter {
   struct Config : public config_utilities::Config<Config> {
     int verbosity = 4;
 
-    std::string ground_truth_pointcloud_file = "";
     std::string output_suffix = "_eval_data.txt";
     bool is_single_tsdf = true;
     bool refine_alignment = false;
@@ -53,8 +52,8 @@ class EvaluationDataExporter {
   virtual ~EvaluationDataExporter() = default;
 
   bool exportEvaluationDataCallback(
-      panoptic_mapping_msgs::SaveLoadMap::Request& request,
-      panoptic_mapping_msgs::SaveLoadMap::Response& response);
+      active_panoptic_mapping_utils::ExportEvaluationData::Request& request,
+      active_panoptic_mapping_utils::ExportEvaluationData::Response& response);
 
  private:
   void setupMembers();
@@ -62,6 +61,7 @@ class EvaluationDataExporter {
 
   bool exportVertexLabels(
       const std::shared_ptr<panoptic_mapping::SubmapCollection>& submaps,
+      std::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> gt_cloud_ptr,
       const Eigen::Matrix4d& alignment_transformation,
       const std::filesystem::path& vertex_labels_file_path,
       const std::optional<std::unordered_map<int, int>>& id_remapping =
@@ -72,7 +72,8 @@ class EvaluationDataExporter {
       const std::filesystem::path& mesh_file_path);
 
   Eigen::Matrix4d computeAlignmentTransformation(
-      const std::shared_ptr<panoptic_mapping::SubmapCollection>& submaps);
+      const std::shared_ptr<panoptic_mapping::SubmapCollection>& submaps,
+      std::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> gt_cloud_ptr);
 
   const Config config_;
   ros::NodeHandle nh_;
@@ -80,7 +81,6 @@ class EvaluationDataExporter {
 
   ros::ServiceServer export_evaluation_data_server_;
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr gt_cloud_ptr_;
   Eigen::Matrix4d default_alignment_transformation_;
 };
 
